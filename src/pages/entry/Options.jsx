e5ab4e748174ffe97e52f6b70806fb208270105c
Breno Lemos <brenomiros@gmail.com>
2021-06-08 13:49:04 -0300

    Options and ScoopOption components

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,3 +1,30 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Row from 'react-bootstrap/Row';
+
+import ScoopOption from './ScoopOption';
+
 export default function Options({ optionType }) {
-  return <div />;
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const { data } = await axios.get(`http://localhost:3030/${optionType}`);
+      setItems(data);
+    };
+
+    loadData();
+  }, [optionType]);
+
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
 }
